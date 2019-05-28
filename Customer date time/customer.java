107f43b167195eddeb6e343ae0de49230976a68f
sidgupta2106 <38638955+sidgupta2106@users.noreply.github.com>
2019-05-28 18:39:39 +0530

    Update customer.java

--- Customer date time/customer.java
+++ Customer date time/customer.java
@@ -2,37 +2,38 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 class customer
 {
-String name;
-String date;
-public void read()
-{
-Scanner input =new Scanner(System.in);
-name=input.next();
-date=input.next();
-}
-public void display()
-{
-System.out.print(name+",");
-String delims="/";
-StringTokenizer st=new StringTokenizer(date,delims);
-while(st.hasMoreElements()){
-System.out.print(st.nextElement()+",");
-}
-System.out.println();
-}
-public static void main(String[] args)
-{
-System.out.println("Enter the customer detail");
-customer[] cus=new customer[30];
-Scanner sc =new Scanner(System.in);
-System.out.println("enter the number of customer");
-int n=sc.nextInt();
-for(int i=0;i<n;i++)
-{
-cus[i]=new customer();
-cus[i].read();
-}
-for(int i=0;i<n;i++)
-cus[i].display();
-}
+  String name;
+  String date;
+  public void read()
+  {
+    Scanner input =new Scanner(System.in);
+    name=input.next();
+    date=input.next();
+  }
+  public void display()
+  {
+    System.out.print(name+",");
+    String delims="/";
+    StringTokenizer st=new StringTokenizer(date,delims);
+    while(st.hasMoreElements())
+    {
+      System.out.print(st.nextElement()+",");
+    }
+   System.out.println();
+  }
+  public static void main(String[] args)
+  {
+    System.out.println("Enter the customer detail");
+    customer[] cus=new customer[30];
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter the number of customer");
+    int n=sc.nextInt();
+    for(int i=0;i<n;i++)
+    {
+      cus[i]=new customer();
+      cus[i].read();
+    }
+    for(int i=0;i<n;i++)
+     cus[i].display();
+  }
 }
